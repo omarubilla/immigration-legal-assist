66b7e8eb67d2e941cdaa408fa82bd72368055eb0
Kevin Ubilla <omar.ubilla@berkeley.edu>
2026-03-26 00:50:48 -0700

    Skip Clerk middleware when Clerk keys are missing

--- proxy.ts
+++ proxy.ts
@@ -1,4 +1,9 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+const hasClerkEnv = Boolean(
+  process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY && process.env.CLERK_SECRET_KEY
+);
 
 const isProtectedRoute = createRouteMatcher([
   "/checkout",
@@ -7,11 +12,15 @@ const isProtectedRoute = createRouteMatcher([
   "/checkout/success",
 ]);
 
-export default clerkMiddleware(async (auth, req) => {
+const protectedClerkMiddleware = clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) {
     await auth.protect();
   }
 });
+
+export default hasClerkEnv
+  ? protectedClerkMiddleware
+  : () => NextResponse.next();
 
 export const config = {
   matcher: [
